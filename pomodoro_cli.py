f2f17fea2fcd2fe8988b1f063acd1e8b71e08643
Stephen B <stephenb.k101010@null.net>
2022-02-01 17:25:44 -0700

    Basic program is done.

--- pomodoro_cli.py
+++ pomodoro_cli.py
@@ -29,30 +29,35 @@ def pomodoro(number_work: int, wk_minutes:int, sb_minutes: int, lb_minutes: int)
     countdown_timer(work_length(wk_minutes))
     countdown_timer(long_break_length(lb_minutes))
 
-#If there is an error it will try again but it won't assign the value to number_input.
-#number_input just ends up being "None".
-def check_input(input_text: str, min_number: int):
-    try:
-        number_input = int(input(input_text))
-        if number_input >= min_number:
-            return number_input
+def get_input(inp_txt: str, min_num: int):
+    usr_in = input(inp_txt)
+    results =check_input(usr_in, min_num)
+    if results[0] == True:
+        return results[1]
+    elif isinstance(results[1], str):
+        print(f"You entered {results[1]}, a string. Please enter an integer greater than {min_num}.")
+        return get_input(inp_txt, min_num)
+    else:
+        print(f"Invalid number. You entered {results[1]}. Please enter an integer greater than {min_num}")
+        return get_input(inp_txt, min_num)
+
+def check_input(usr_in, min_number: int):
+    if usr_in.isdigit():
+        usr_num = int(usr_in)
+        if usr_num >= min_number:
+            return (True, usr_num)
         else:
-            print(f"Please enter a whole number greater than or equal to {min_number}")
-    except ValueError:
-        print("Invalid entry. Please use positive non-zero integers.")
-    except Exception as e:
-        print("Invalid entry.")
-        print(type(e))
-    if type(number_input) != "int":
-        check_input(input_text, min_number)
+            return (False, usr_num)
+    else:
+        return (False, usr_in)
 
 def main():
     print("Welcome to CL Pomodoro Timer!")
-    number_work = check_input("Number of work sessions before long break (at least 2): ", 2)
+    number_work = get_input("Number of work sessions before long break (at least 2): ", 2)
     print("Please enter the number of minutes for each timer:")
-    wk_minutes = check_input("Work Session length: ", 1)
-    sb_minutes = check_input("Break length: ", 1)
-    lb_minutes = check_input("Long break length: ", 1)
+    wk_minutes = get_input("Work Session length: ", 1)
+    sb_minutes = get_input("Break length: ", 1)
+    lb_minutes = get_input("Long break length: ", 1)
     pomodoro(number_work, wk_minutes, sb_minutes, lb_minutes)
 
 main()
